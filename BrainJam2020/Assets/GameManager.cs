e195adab81d654715f1d87eb7b3138b4ba576912
TimO-F42 <tim.ogunmakin@factory42.uk>
2020-02-01 21:47:19 +0000

    Level manager

---
+++ BrainJam2020/Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : GenericSingleton<GameManager>
+{
+    public List<string> levels = new List<string>();
+
+    public int _currentLevel = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levels.Add("ArtScene");
+        levels.Add("level1");
+        levels.Add("level2");
+        levels.Add("level3");
+        levels.Add("level4");
+        levels.Add("level5");
+        SceneManager.LoadScene(levels[_currentLevel], LoadSceneMode.Additive);
+    }
+
+    public void MoveToNextLevel()
+    {
+        SceneManager.UnloadSceneAsync(levels[_currentLevel]);
+        _currentLevel++;
+        SceneManager.LoadScene(levels[_currentLevel]);
+    }
+
+}
--- BrainJam2020/Assets/Scripts/Game.cs
+++ BrainJam2020/Assets/Scripts/Game.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    public float completeDuration;
+    private float completeTimer;
+    private bool readyForCompletion;
+
     public enum ViewState
     {
         GOD,
@@ -11,15 +16,25 @@ public class Game : MonoBehaviour
     }
 
     public ViewState _viewState;
-    // Start is called before the first frame update
-    void Start()
+
+    public void LoadNextLevel()
     {
-        
+        readyForCompletion = true;
+        GameManager.Instance.MoveToNextLevel();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (readyForCompletion)
+        {
+            completeTimer += Time.deltaTime;
+
+            float norm = completeTimer / completeDuration;
+
+            if (norm >= 1.0f)
+            {
+                LoadNextLevel();
+            }
+        }
     }
 }
