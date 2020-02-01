e83d89dd6cccef11f6554af5fc3651c911f251e6
TimO-F42 <tim.ogunmakin@factory42.uk>
2020-02-01 16:53:29 +0000

    Planet selector

--- BrainJam2020/Assets/PlanetSelector.cs
+++ BrainJam2020/Assets/PlanetSelector.cs
@@ -10,18 +10,38 @@ public class PlanetSelector : MonoBehaviour
 
     public GodCameraFollow _camFollow;
     public GodCamera _camHandler;
+
+    private Material _material;
+    
     // Update is called once per frame
     void Update()
     {
         _ray = _godCamera.ScreenPointToRay(Input.mousePosition);
 
-        if (Input.GetMouseButtonDown(0))
+        if (Physics.Raycast(_ray, out _hit))
         {
-            if (Physics.Raycast(_ray, out _hit))
+            if (Input.GetMouseButtonDown(0))
             {
                 _camFollow.target = _hit.transform;
                 _camHandler.target = _hit.transform;
             }
+
+            if (_hit.transform.GetComponent<MeshRenderer>())
+            {
+                if (_material != _hit.transform.GetComponent<MeshRenderer>().material)
+                {
+                    if (_material)
+                    {
+                        _material.SetInt("isHighlighted",0);
+                    }
+                    
+                    _material = _hit.transform.GetComponent<MeshRenderer>().material;
+                    _hit.transform.GetComponent<MeshRenderer>().material.SetInt("isHighlighted",1);
+                }
+            }
+            
         }
+        
+        
     }
 }
