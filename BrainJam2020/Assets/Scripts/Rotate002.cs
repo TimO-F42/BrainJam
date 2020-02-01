c30f40739fd6a10199cc190423c7c5b84d385543
niccolo-f42 <57440469+niccolo-f42@users.noreply.github.com>
2020-02-01 20:34:58 +0000

    More stuff ART

---
+++ BrainJam2020/Assets/Scripts/Rotate002.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate002: MonoBehaviour
+{
+
+    public float speed = 20.0f;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+        transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+
+    }
+}
