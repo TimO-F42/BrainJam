fc820accf7a8b65a11b05a53a78de4fe62c96d67
TimO-F42 <tim.ogunmakin@factory42.uk>
2020-02-02 17:59:41 +0000

    fixed physics

--- BrainJam2020/Assets/Scripts/LandOnPlanet.cs
+++ BrainJam2020/Assets/Scripts/LandOnPlanet.cs
@@ -12,19 +12,37 @@ public class LandOnPlanet : MonoBehaviour
     public bool landed;
 
     public UnityEvent _event;
+
+    public bool isTarget;
     
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<Player>())
+        if (!isTarget)
         {
-            other.GetComponent<Player>().ToggleRagdollDisabled(false);
+            if (other.GetComponent<Player>())
+            {
+                other.GetComponent<Player>().ToggleRagdollDisabled(false);
+            }
         }
+        else
+        {
+            
+        }
+        
     }
     
     public virtual void OnTriggerStay(Collider other)
     {
+        if (other.gameObject.name == "Goal_Prefab") return;
         if (other.GetComponent<Player>())
         {
+            if (isTarget)
+            {
+                other.GetComponent<Player>().nearTarget = true;
+                other.GetComponent<Player>().transform.position =
+                    Vector3.Lerp(other.GetComponent<Player>().transform.position, transform.position, Time.deltaTime * 0.2f);
+            }
+            
             timer += Time.deltaTime;
 
             float completeNorm = timer / landDuration;
