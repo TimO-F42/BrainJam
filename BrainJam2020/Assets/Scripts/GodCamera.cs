cc8e9117d574b516c85524f5354f8027ee058975
Username-Leon <leonardo.temperanza@gmail.com>
2020-02-02 22:40:07 +0100

    Camera changes

--- BrainJam2020/Assets/Scripts/GodCamera.cs
+++ BrainJam2020/Assets/Scripts/GodCamera.cs
@@ -5,53 +5,47 @@ using UnityEngine;
 public class GodCamera : MonoBehaviour
 {
     public Transform target;
-    public float turnSpeed;
+    public float turnSpeed = 70f;
     //private Vector3 offset;
-    
-    public float height = 1f;
-    public float distance = 2f;
-    
-   //private Vector3 offsetX;
-   // private Vector3 offsetY;
-    
-    
-    
+
+    private float distance;
+    private float rotationX;
+    private float rotationY;
+
+    //private Vector3 offsetX;
+    // private Vector3 offsetY;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         //offsetX = new Vector3 (0, height, distance);
         //offsetY = new Vector3 (0, 0, distance);
-        
+
         //offset = new Vector3(target.position.x, target.position.y + 8.0f, target.position.z + 7.0f);
+
+        rotationX = transform.rotation.eulerAngles.y;
+        rotationY = transform.rotation.eulerAngles.x;
+
+        distance = (transform.parent.position - transform.position).magnitude;
+        transform.LookAt(target);
     }
 
     // Update is called once per frame
     void Update()
     {
     }
-    
+
     void LateUpdate()
     {
-        if(Input.GetKey (KeyCode.A))
-        {
-            transform.RotateAround(target.position, Vector3.up, turnSpeed * Time.deltaTime);
-        }
-        
-        if(Input.GetKey (KeyCode.D))
-        {
-            transform.RotateAround(target.position, -Vector3.up, turnSpeed * Time.deltaTime);
-        }
+        Vector2 input = new Vector2(Input.GetAxis("Camera X") * turnSpeed * Time.deltaTime, Input.GetAxis("Camera Y") * turnSpeed * Time.deltaTime);
 
-        if(Input.GetKey (KeyCode.W))
-        {
-            transform.RotateAround(target.position, -Vector3.right, turnSpeed * Time.deltaTime);
-        }
-        
-        if(Input.GetKey (KeyCode.S))
-        {
-            transform.RotateAround(target.position, Vector3.right, turnSpeed * Time.deltaTime);
-        }
-        
-        transform.LookAt(target);
+        rotationX = (rotationX + input.x) % 360f;
+        rotationY = (rotationY + input.y) % 360f;
+
+        transform.localRotation = Quaternion.Euler(rotationY, rotationX, 0f);
+
+        transform.position = target.position - transform.forward * distance;
     }
 }
